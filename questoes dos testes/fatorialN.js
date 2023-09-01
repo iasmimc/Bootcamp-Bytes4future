abe5be511d4462dd6caebe73359dbe060b84d45e
Iasmim Capra <iasmim.capra@gmail.com>
2023-09-01 11:06:35 +0100

    fatorialN

---
+++ questoes dos testes/fatorialN.js
@@ -0,0 +1,22 @@
+// Cria uma função que recebe como argumento um número inteiro de qualquer dimensão e
+// retorna o fatorial desse número.
+// Utiliza o que aprendeste sobre while para resolver este exercício.
+function fatorialN(n) {
+  if (n < 0) {
+    return "Não é possível calcular o fatorial de um número negativo.";
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let resultado = 1;
+    while (n > 1) {
+      resultado *= n;
+      n--;
+    }
+    return resultado;
+  }
+}
+
+// Exemplo de uso:
+const numero = 5;
+const fatorial = fatorialN(numero);
+console.log(`O fatorial de ${numero} é ${fatorial}`);
